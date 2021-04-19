03c4a4f173a2085f762098cb1e1d93bbedcf2eea
Qingzhao <qcao5@ucsc.edu>
2021-04-20 07:09:19 +0800

    message

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+//Name: Qingzhao Cao
+//Project title: RocketPatrol Mod
+//2021.4.19
+//LIKE 13 HOURS TO COMPLETE
+//Implement a simultaneous two-player mode (30)
+//Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+//Implement an alternating two-player mode (20)
+//Replace the UI borders with new artwork (10)
+//Create a new title screen (e.g., new artwork, typography, layout) (10)
+
+//TA Ishaan helped me a lot. Thank you so much.
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
     height: 480,
-    scene: [ Menu, Play1, Play2 ]
+    scene: [ Menu, Play1, Play2 ],
 };
 
 let game = new Phaser.Game(config);
