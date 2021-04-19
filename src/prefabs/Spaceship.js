457e06e0a7aa70ce9acc3405cb8a976a854580c2
Qingzhao <qcao5@ucsc.edu>
2021-04-20 05:31:56 +0800

    test

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,4 +1,5 @@
 //Spaceship prefabs
+// test
 class Spaceship extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, texture, frame, pointValue){
         super(scene, x, y, texture, frame)
